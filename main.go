7f2d6cfeff6c107656316eab0e7dd2f510b1ba49
vanh <vietahh2002@gmail.com>
2023-11-20 15:07:30 +0700

    New update

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func main() {
+	tpl, _ = template.ParseFiles("index.html", "home.html")
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/bye", bye)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.html", nil)
+}
+
+func bye(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "home.html", nil)
+}
