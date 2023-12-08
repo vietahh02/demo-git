09daa9581d3eb84f9c11fe1a0d364093d2b7368d
vanh <vietahh2002@gmail.com>
2023-12-08 14:46:23 +0700

    end

--- main.go
+++ main.go
@@ -13,15 +13,20 @@ var db *sql.DB
 var tpl *template.Template
 
 type Result struct {
-	T string
-	F string
+	ListCate []Category
+	F        string
+}
+
+type Category struct {
+	IdCate   string `json:"id`
+	NameCate string `json:"name"`
 }
 
 func main() {
 	tpl, _ = tpl.ParseGlob("templates/*.html")
 	var err error
 	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-	db, err = sql.Open("mysql", "root:123@tcp(localhost:3306)/shopcart")
+	db, err = sql.Open("mysql", "root:1234567890@tcp(localhost:3306)/shopcart")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -34,5 +39,19 @@ func main() {
 
 func home(w http.ResponseWriter, r *http.Request) {
 
-	tpl.ExecuteTemplate(w, "1test.html", nil)
+	var send Result
+	send.ListCate = listCategories()
+	send.F = "had"
+	tpl.ExecuteTemplate(w, "header.html", send)
+}
+
+func listCategories() []Category {
+	rows, _ := db.Query("SELECT idCate, NameCate FROM shopcart.categories")
+	var c1 Category
+	var ListCate []Category
+	for rows.Next() {
+		rows.Scan(&c1.IdCate, &c1.NameCate)
+		ListCate = append(ListCate, c1)
+	}
+	return ListCate
 }
