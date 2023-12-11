f26d62e832e0bf12d26000e84bddb8edad05d2f4
vanh <vietahh2002@gmail.com>
2023-12-11 16:27:25 +0700

    Ad

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 	tpl, _ = tpl.ParseGlob("templates/*.html")
 	var err error
 	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-	db, err = sql.Open("mysql", "root:1234567890@tcp(localhost:3306)/shopcart")
+	// db, err = sql.Open("mysql", "root:1234567890@tcp(localhost:3306)/shopcart")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -39,19 +39,20 @@ func main() {
 
 func home(w http.ResponseWriter, r *http.Request) {
 
-	var send Result
-	send.ListCate = listCategories()
-	send.F = "had"
-	tpl.ExecuteTemplate(w, "header.html", send)
+	// var send Result
+	// send.ListCate = listCategories()
+	// send.F = "had"
+	// tpl.ExecuteTemplate(w, "header.html", send)
+	tpl.ExecuteTemplate(w, "1test.html", nil)
 }
 
-func listCategories() []Category {
-	rows, _ := db.Query("SELECT idCate, NameCate FROM shopcart.categories")
-	var c1 Category
-	var ListCate []Category
-	for rows.Next() {
-		rows.Scan(&c1.IdCate, &c1.NameCate)
-		ListCate = append(ListCate, c1)
-	}
-	return ListCate
-}
+// func listCategories() []Category {
+// 	rows, _ := db.Query("SELECT idCate, NameCate FROM shopcart.categories")
+// 	var c1 Category
+// 	var ListCate []Category
+// 	for rows.Next() {
+// 		rows.Scan(&c1.IdCate, &c1.NameCate)
+// 		ListCate = append(ListCate, c1)
+// 	}
+// 	return ListCate
+// }
